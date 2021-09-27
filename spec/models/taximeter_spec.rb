abd1226e18d61f102db36ef9e8746b41f96965fb
Luis M <lgmm.93@gmail.com>
2021-09-27 14:38:16 -0600

    changes in views

--- spec/models/taximeter_spec.rb
+++ spec/models/taximeter_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Taximeter, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it {should belong_to :company}
+
+  it { should validate_presence_of :start_price}
+  it { should validate_presence_of :km_price}
+  it { should validate_presence_of :minute_price}
+  it { should validate_presence_of :minimum_price}
+  it { should validate_presence_of :time_from}
+  it { should validate_presence_of :time_to}
+  it { should validate_presence_of :days}
 end
